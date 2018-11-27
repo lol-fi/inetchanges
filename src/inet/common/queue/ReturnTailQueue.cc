8f904bda2f305c62626a52509ecab4a8ea09c393
Robin Brooks <sapricotzine@gmail.com>
2018-11-27 15:54:06 -0500

    initial commit with return tail queue in inet

---
+++ src/inet/common/queue/ReturnTailQueue.cc
@@ -0,0 +1,87 @@
+//
+// Copyright (C) 2005 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "inet/common/INETDefs.h"
+
+#include "inet/common/queue/ReturnTailQueue.h"
+
+namespace inet {
+
+Define_Module(ReturnTailQueue);
+
+simsignal_t ReturnTailQueue::queueLengthSignal = registerSignal("queueLength");
+
+void ReturnTailQueue::initialize()
+{
+    PassiveQueueBase::initialize();
+
+    queue.setName(par("queueName"));
+
+    //statistics
+    emit(queueLengthSignal, queue.getLength());
+
+    outGate = gate("out");
+
+    // configuration
+    frameCapacity = par("frameCapacity");
+
+}
+
+cMessage *ReturnTailQueue::enqueue(cMessage *msg)
+{
+    //drop beyond threshold
+    if (frameCapacity && queue.getLength() >= frameCapacity) {
+        EV << "Queue full, returning packet.\n";
+        return msg;
+    }
+    else {
+        queue.insert(msg);
+        emit(queueLengthSignal, queue.getLength());
+        return nullptr;
+    }
+}
+
+cMessage *ReturnTailQueue::dequeue()
+{
+    if (queue.isEmpty())
+        return nullptr;
+
+    cMessage *msg = static_cast<cMessage *>(queue.pop());
+
+    // statistics
+    emit(queueLengthSignal, queue.getLength());
+
+    return msg;
+}
+
+void ReturnTailQueue::sendOut(cMessage *msg)
+{
+    send(msg, outGate);
+}
+
+int ReturnTailQueue::length()
+{
+    return queue.getLength();
+}
+
+bool ReturnTailQueue::isEmpty()
+{
+    return queue.isEmpty();
+}
+
+} // namespace inet
+
